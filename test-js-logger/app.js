4c94f76b6d5c35f6c5ccf50d78cde5a7b9f68c01
Arkadiusz Komarzewski <akomarzewski@mozilla.com>
2023-06-22 19:00:11 +0200

    Use real Glean event payload

--- test-js-logger/app.js
+++ test-js-logger/app.js
@@ -24,9 +24,45 @@ setInterval(() => {
     console.log('Logging something every second');
     const timestamp = new Date().toISOString();
 
+    
+    let eventPayload = `{
+      "metrics": {
+        "string": {
+          "event.name": "reg_submit_success",
+          "account.user_id_sha256": "",
+          "relying_party.oauth_client_id": "",
+          "relying_party.service": "sync",
+          "session.device_type": "desktop",
+          "session.entrypoint": "",
+          "session.flow_id": "5d1eaf933f521cb2a15af909c813673ada8485d6ace8e806c57148cd7f13b30c",
+          "utm.campaign": "",
+          "utm.content": "",
+          "utm.medium": "",
+          "utm.source": "",
+          "utm.term": ""
+        }
+      },
+      "ping_info": {
+        "seq": 2,
+        "start_time": "2023-06-22T11:28-05:00",
+        "end_time": "2023-06-22T11:28-05:00"
+      },
+      "client_info": {
+        "telemetry_sdk_build": "1.4.0",
+        "client_id": "73f5ae86-90f8-46a3-9ccc-bca902a175bf",
+        "first_run_date": "2023-06-22-05:00",
+        "os": "Darwin",
+        "os_version": "Unknown",
+        "architecture": "Unknown",
+        "locale": "en-US",
+        "app_build": "Unknown",
+        "app_display_version": "0.0.0",
+        "app_channel": "development"
+      }
+    }`;
+    eventPayload = JSON.stringify(JSON.parse(eventPayload));
     // this outputs escaped payload and is similar to how FxA logs events right now: https://github.com/mozilla/fxa/blob/6d65d20d5cdd7e70f29f1c6e3d44185f0d06f117/packages/fxa-auth-server/lib/log.js#L289
     // {"Timestamp":1687448048064000000,"Logger":"fxa-oauth-server","Type":"glean-server-event","Severity":6,"Pid":33056,"EnvVersion":"2.0","Fields":{"payload":"{\"timestamp\": \"2023-06-22T15:34:08.058Z\", \"event\": \"test\"}"}}
-    const eventPayload = `{"timestamp": "${timestamp}", "event": "test"}`;
     log.info('glean-server-event', {payload: eventPayload});
     
     // this outputs payload as a nested JSON so it will be automatically parsed by BQ sink
