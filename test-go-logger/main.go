314afa2ea9ba2aa14f98419f1f2382da536d7cec
Arkadiusz Komarzewski <akomarzewski@mozilla.com>
2023-12-20 10:37:47 +0100

    Add generated code

--- test-go-logger/main.go
+++ test-go-logger/main.go
@@ -1,13 +1,29 @@
 package main
 
 import (
-    "log"
-    "time"
+	"test-go-logger/glean"
+	"time"
 )
 
 func main() {
-    for {
-        log.Println("Logging in a loop...")
-        time.Sleep(time.Second)
-    }
+	for {
+		logger := glean.GleanEventsLogger{
+			AppID:             "test-go-logger",
+			AppDisplayVersion: "0.0.1",
+			AppChannel:        "nightly",
+		}
+
+		requestInfo := glean.RequestInfo{
+		}
+
+		params := glean.EventBackendObjectUpdate{
+			IdentifiersFxaAccountId: "your_account_id",
+			ObjectType:              "your_object_type",
+			ObjectState:             "your_object_state",
+		}
+
+		logger.RecordEventBackendObjectUpdate(requestInfo, params)
+
+		time.Sleep(time.Second)
+	}
 }
